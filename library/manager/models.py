07b09f9c6c920b482b8eaa047c267fd91e5ee0b1
Shreyash Ingle <141054759+shreyash0019@users.noreply.github.com>
2025-04-12 16:17:03 +0530

    models.py

--- library/manager/models.py
+++ library/manager/models.py
@@ -16,7 +16,7 @@ class Book(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)  
 
     def __str__(self):
-        return self.title  # ✅ use 'title', not 'name'
+        return self.title  
 
 
 class BorrowRecord(models.Model):
